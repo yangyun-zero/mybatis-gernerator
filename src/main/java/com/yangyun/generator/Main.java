565f08a027293d54f1eadda4fff705fdbb499484
yangyun <yy502119341@163.com>
2020-10-12 11:48:26 +0800

    -

--- src/main/java/com/yangyun/generator/Main.java
+++ src/main/java/com/yangyun/generator/Main.java
@@ -21,7 +21,7 @@ import java.util.List;
  */
 public class Main {
 
-    private static String[] TABLES = {"shipping_address_info"};
+    private static String[] TABLES = {"4444444"};
 
     private static String C = "_";
 
